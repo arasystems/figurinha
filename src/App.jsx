44a0f0a4e879b0b831547c053b436ec240f2dedd
arasystems <arajaortiz@gmail.com>
2026-05-14 09:54:07 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -546,7 +546,7 @@ const styles = {
 
   // Upload zone
   uploadZone: {
-    border: '2px dashed rgba(0, 0, 0, 0.2)',
+    border: '2px dashed rgba(0, 0, 0, 0)',
     borderRadius: 12,
     padding: '28px 20px',
     textAlign: 'center',
